0fa2e2e64b36220be4e6383965f7e856ec1f0ce8
Bruno Euflausino <brunoeuflausino@gmail.com>
2025-10-27 16:18:12 -0300

    fix exceptions

--- src/main/java/com/euflausino/desafiotodo/dto/TodoRequestDTO.java
+++ src/main/java/com/euflausino/desafiotodo/dto/TodoRequestDTO.java
@@ -1,15 +1,16 @@
 package com.euflausino.desafiotodo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.lang.NonNull;
 
 public record TodoRequestDTO(
-        @NotBlank
+        @NotBlank(message = "{nome.obrigatorio}")
         String nome,
 
         String descricao,
 
-        @NonNull
+        @NotNull(message = "{prioridade.obrigatorio}")
         Integer prioridade
 
 ) {
--- src/main/java/com/euflausino/desafiotodo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/euflausino/desafiotodo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.euflausino.desafiotodo.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
@@ -10,20 +13,27 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleGenericException(Exception ex) {
-        return new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        return ResponseEntity.internalServerError().body(new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldErrors().get(0);
+        String message = fieldError.getDefaultMessage();
+        return ResponseEntity.badRequest().body(new ErrorResponse(message, HttpStatus.BAD_REQUEST.value()));
     }
 
     @ExceptionHandler(TodoNaoEncontradaException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse UsuarioNotFound(Exception ex) {
-        return new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+    public ResponseEntity<ErrorResponse> UsuarioNotFound(Exception ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
 }
 
