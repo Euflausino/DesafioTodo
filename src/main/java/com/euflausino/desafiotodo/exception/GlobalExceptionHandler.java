1638c085380a67c39d6e70f78e8a9f1777abe2fb
Bruno Euflausino <brunoeuflausino@gmail.com>
2025-10-29 19:37:02 -0300

    adicionando mais config de seguranca

--- src/main/java/com/euflausino/desafiotodo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/euflausino/desafiotodo/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
