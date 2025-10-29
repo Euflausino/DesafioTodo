5ee2252a1d42cf5b42b964baa4bcd96a7ecdd992
Bruno Euflausino <brunoeuflausino@gmail.com>
2025-10-29 19:56:31 -0300

    Tratando exceções na service

--- src/main/java/com/euflausino/desafiotodo/service/TodoService.java
+++ src/main/java/com/euflausino/desafiotodo/service/TodoService.java
@@ -11,8 +11,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class TodoService {
 
@@ -36,17 +34,14 @@ public class TodoService {
 
     @Transactional
     public Page<TodoResponseDTO> excluir(Long id) {
-        if (!todoRepository.existsById(id)) {
-            throw new TodoNaoEncontradaException("Usuário não encontrado.");
-        }
+        validaTodo(id);
         todoRepository.deleteById(id);
         return listarTodos();
     }
 
     @Transactional
     public TodoResponseDTO editarTodo(Long id, AtualizaTodoRequestDTO todoRequestDTO) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new TodoNaoEncontradaException("Todo não encontrada."));
+        Todo todo =  validaTodo(id);
 
         if(todoRequestDTO.nome() != null) todo.setNome(todoRequestDTO.nome());
         if(todoRequestDTO.descricao() != null) todo.setDescricao(todoRequestDTO.descricao());
@@ -57,8 +52,12 @@ public class TodoService {
     }
 
     public TodoResponseDTO getTodo(Long id) {
-        todoRepository.findById(id).orElseThrow(() -> new TodoNaoEncontradaException("Todo não encontrada."));
-        return TodoMapper.todoResponseDTO(todoRepository.findById(id).get());
+
+        return TodoMapper.todoResponseDTO(validaTodo(id));
+    }
+
+    private Todo validaTodo(Long id){
+        return todoRepository.findById(id).orElseThrow(() -> new TodoNaoEncontradaException("Todo não encontrada"));
     }
 
 }
